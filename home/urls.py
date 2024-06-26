f78cb71af8c3e212f3821e8c9536e2845cb47e5a
onkar <onkarsawant111@gmail.com>
2024-06-26 23:14:25 +0530

    added login app and login page and gave routing to it

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/aboutus', views.aboutus, name='aboutus'),
+    path('aboutus', views.aboutus, name='aboutus'),
 ]
--- petfam/settings.py
+++ petfam/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    'login.apps.LoginConfig',
 ]
 
 MIDDLEWARE = [
