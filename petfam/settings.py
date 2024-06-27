b427e18c64fe491fa83eab2eff902edeceb8b8bf
onkar <onkarsawant111@gmail.com>
2024-06-27 15:50:46 +0530

    django is not working

--- petfam/settings.py
+++ petfam/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'petfam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
